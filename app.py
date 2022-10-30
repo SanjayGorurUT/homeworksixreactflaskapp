45d939afe6d9abf8e74fcced03fee1dcd15ad420
Sanjay Gorur <sanjaygorur@wireless-10-145-56-7.public.utexas.edu>
2022-10-30 17:43:51 -0500

    Reverted app

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-from flask import Flask
-from UserBackend import UserBackend
-from ProjectBackend import ProjectBackend
-from HardwareBackend import HardwareBackend
+import os
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
-    
-@app.route('/users')
-def users():
-    return {"users": ["user1", "user2", "user3"]}
-
-@app.route('/projects')
-def projects():
-    return "projects"
-
-@app.route('/hwsets')
-def hwsets():
-    hardwareBackend = HardwareBackend()
-    return "hwsets"
+app = Flask(__name__, static_url_path='', static_folder='ui/build/')
 
 @app.route('/')
-def hwsets():
-    return ""
+def index():
+    return app.send_static_file('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/checkin/<projectname>/<qty>')
+def checkIn_hardware(projectname, qty):
+    return str(qty) + " was checked in to " + str(projectname)
+
+@app.route('/checkout/<projectname>/<qty>')
+def checkOut_hardware(projectname, qty):
+    return str(qty) + " was checked out from " + str(projectname)
+
+@app.route('/join/<projectname>')
+def joinProject(projectname):
+    return "Sanjay Gorur joined " + str(projectname)
+
+@app.route('/leave/<projectname>')
+def leaveProject(projectname):
+    return "Sanjay Gorur left " + str(projectname)
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
